646b08a8db0c471d29a6a394ba2e6f1b130021fa
Perri Lapidus <perri@wondersauce.com>
2015-04-02 18:22:38 -0400

    show views

--- app/overrides/admin_user_sub_menu.rb
+++ app/overrides/admin_user_sub_menu.rb
@@ -1,8 +1,8 @@
-# Deface::Override.new(
-#   virtual_path: "spree/admin/shared/_menu",
-#   name: "admin_user_sub_menu_index",
-#   insert_after: "#admin-menu",
-#   partial: "spree/admin/users/sub_menu",
-# )
+Deface::Override.new(
+  virtual_path: "spree/admin/shared/_menu",
+  name: "admin_user_sub_menu_index",
+  insert_after: "#admin-menu",
+  partial: "spree/admin/users/sub_menu",
+)
 
 # SO I CAN MERGE INTO DEVELOP 4/2 --PSL
