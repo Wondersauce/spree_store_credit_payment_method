d3b01e7ac59a70baf59eafc2fb319329733b1712
Perri Lapidus <perri@wondersauce.com>
2015-04-02 18:56:12 -0400

    update payment method decorator

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,14 +1,16 @@
-module SpreeStoreCredits::PaymentMethodDecorator
+module Spree
+	PaymentMethod.class_eval do:
 
-  def self.included(base)
-    base.prepend(InstanceMethods)
-  end
+	  def self.included(base)
+	    base.prepend(InstanceMethods)
+	  end
 
-  module InstanceMethods
-    def store_credit?
-      self.class == Spree::PaymentMethod::StoreCredit
-    end
-  end
+	  module InstanceMethods
+	    def store_credit?
+	      self.class == Spree::PaymentMethod::StoreCredit
+	    end
+	  end
+	end
 end
 
-Spree::PaymentMethod.include SpreeStoreCredits::PaymentMethodDecorator
+# Spree::PaymentMethod.include SpreeStoreCredits::PaymentMethodDecorator
