807c2a904de4f66422934ebd80812d33bd1707dd
Perri Lapidus <perri@wondersauce.com>
2015-04-02 15:27:27 -0400

    hide gift cards, apply store credit on confirm

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@ Spree::PaymentMethod.find_or_create_by(type: "Spree::PaymentMethod::StoreCredit"
 Spree::StoreCreditType.find_or_create_by(name: 'Expiring', priority: 1)
 Spree::StoreCreditType.find_or_create_by(name: 'Non-expiring', priority: 2)
 
-Spree::StoreCreditCategory.find_or_create_by(name: 'Gift Card')
+# Spree::StoreCreditCategory.find_or_create_by(name: 'Gift Card')
+Spree::StoreCreditCategory.find_or_create_by(name: 'Friends and Family')
